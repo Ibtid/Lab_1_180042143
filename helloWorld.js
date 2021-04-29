075d52caec465804c12887da26b76a5a03553616
Ibtid <ibtid011@example.com>
2021-04-30 00:02:33 +0600

    completed util module

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+module.exports.Hello = () => {
+  console.log('hello');
+};
+
+module.exports.name = 'Ibtid Rahman';
+
+//module.exports = { Hello, name };
---
+++ util.js
@@ -0,0 +1,18 @@
+const HelloFunc = require('./helloWorld');
+
+//HelloFunc.Hello();
+
+//console.log(HelloFunc.name);
+
+//setInterval
+setInterval(() => {
+  HelloFunc.Hello();
+}, 1000);
+
+setTimeout(() => {
+  console.log(HelloFunc.name);
+}, 5000);
+
+//local Module
+//Global Module
+//3rd Party Module/Package
